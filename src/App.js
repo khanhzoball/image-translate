cfad5f8441b5e5a673a72c6ee653efd09acc9078
khanhzoball <khanhhnguyen@g.ucla.edu>
2022-03-24 04:51:22 -0700

    added index

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/home.js";
-// import "./components/index.css";
+import "./components/index.css";
 
 function App () {
   return (
