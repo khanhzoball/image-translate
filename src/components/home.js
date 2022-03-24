cd704384ac2edb59715a9df09175f15776f63860
khanhzoball <khanhhnguyen@g.ucla.edu>
2022-03-24 04:50:06 -0700

    updated home

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,47 @@
 import React, { useState, useEffect } from "react";
 
 const Home = () => {
+    
+    const LOG_IMG = (image) => {
+        const file = image[0]
+
+        const reader = new FileReader();
+        reader.onloadend = function() {
+            const data_uri = reader.result
+
+            fetch("/api/image_translate", {
+                method: "POST",
+                body: JSON.stringify({
+                    'data_uri': data_uri,
+                }),
+            })
+            .then(res=>(res.json()))
+            .then(resJson => {
+                let canvas = document.getElementById("result-image");
+                let img = new Image()
+
+                document.getElementById("result-image").src = resJson["new_img"];
+            })
+        }
+        reader.readAsDataURL(file);
+    }
+
+
     return (
         <div>
-            hi
+            <div className="container">
+                <div className="title">
+                    Vision
+                </div>
+                <div>
+                    <img src="" id="result-image" class="image"/>
+                </div>
+                <div>
+                    <input type="file" accept="image/png, image/jpg" onChange={(e) => {
+                        LOG_IMG(e.target.files)
+                    }}/>
+                </div>
+            </div>
         </div>
     )
 }
